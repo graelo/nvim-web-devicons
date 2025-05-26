6d83af74f14a29a8e70f70882b9a17b19f469928
graelo <graelo@grael.cc>
2025-05-26 22:05:10 +0200

    feat: add cterm_color codes

--- lua/nvim-web-devicons/default/icons_by_desktop_environment.lua
+++ lua/nvim-web-devicons/default/icons_by_desktop_environment.lua
@@ -1,10 +1,10 @@
 return {
-  ["budgie"]   = { icon = "", color = "#4E5361", cterm_color = "240", name = "Budgie"    },
-  ["cinnamon"] = { icon = "", color = "#DC682E", cterm_color = "166", name = "Cinnamon"  },
-  ["gnome"]    = { icon = "", color = "#FFFFFF", cterm_color = "231", name = "GNOME"     },
-  ["lxde"]     = { icon = "", color = "#A4A4A4", cterm_color = "248", name = "LXDE"      },
-  ["lxqt"]     = { icon = "", color = "#0191D2", cterm_color = "32",  name = "LXQt"      },
-  ["mate"]     = { icon = "", color = "#9BDA5C", cterm_color = "113", name = "MATE"      },
-  ["plasma"]   = { icon = "", color = "#1B89F4", cterm_color = "33",  name = "KDEPlasma" },
-  ["xfce"]     = { icon = "", color = "#00AADF", cterm_color = "74",  name = "Xfce"      },
+  ["budgie"]   = { icon = "", color = "#4E5361", cterm_color = "0", name = "Budgie"    },
+  ["cinnamon"] = { icon = "", color = "#DC682E", cterm_color = "9", name = "Cinnamon"  },
+  ["gnome"]    = { icon = "", color = "#FFFFFF", cterm_color = "6", name = "GNOME"     },
+  ["lxde"]     = { icon = "", color = "#A4A4A4", cterm_color = "6", name = "LXDE"      },
+  ["lxqt"]     = { icon = "", color = "#0191D2", cterm_color = "4",  name = "LXQt"      },
+  ["mate"]     = { icon = "", color = "#9BDA5C", cterm_color = "2", name = "MATE"      },
+  ["plasma"]   = { icon = "", color = "#1B89F4", cterm_color = "13",  name = "KDEPlasma" },
+  ["xfce"]     = { icon = "", color = "#00AADF", cterm_color = "4",  name = "Xfce"      },
 } --[[@as table<string, Icon>]]
